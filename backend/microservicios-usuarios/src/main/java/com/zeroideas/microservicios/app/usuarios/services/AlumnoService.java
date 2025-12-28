81867829fa554a3fee0ea6cdabfb7bd02f34967c
jckballero <jean.caballero29@hotmail.com>
2025-12-28 01:50:16 -0500

    refactor: Llamada al commons e implementacion en /services/AlumniService e Impl

--- backend/microservicios-usuarios/src/main/java/com/zeroideas/microservicios/app/usuarios/services/AlumnoService.java
+++ backend/microservicios-usuarios/src/main/java/com/zeroideas/microservicios/app/usuarios/services/AlumnoService.java
@@ -1,16 +1,8 @@
 package com.zeroideas.microservicios.app.usuarios.services;
 
 import com.zeroideas.microservicios.app.usuarios.models.entity.Alumno;
+import com.zeroideas.microservicios.commons.services.CommonService;
 
-import java.util.Optional;
+public interface AlumnoService extends CommonService<Alumno> {
 
-public interface AlumnoService {
-
-    public Iterable<Alumno> findAll();
-
-    public Optional<Alumno> findById(Long id);
-
-    public Alumno save(Alumno alumno);
-
-    public void deleteById(Long id);
 }
--- backend/microservicios-usuarios/src/main/java/com/zeroideas/microservicios/app/usuarios/services/AlumnoServiceImpl.java
+++ backend/microservicios-usuarios/src/main/java/com/zeroideas/microservicios/app/usuarios/services/AlumnoServiceImpl.java
@@ -2,39 +2,11 @@ package com.zeroideas.microservicios.app.usuarios.services;
 
 import com.zeroideas.microservicios.app.usuarios.models.entity.Alumno;
 import com.zeroideas.microservicios.app.usuarios.models.repository.AlumnoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.zeroideas.microservicios.commons.services.CommonServiceImpl;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
-public class AlumnoServiceImpl implements AlumnoService{
+public class AlumnoServiceImpl extends CommonServiceImpl<Alumno, AlumnoRepository> implements AlumnoService{
 
-    @Autowired
-    private AlumnoRepository repository;
 
-    @Override
-    @Transactional(readOnly = true)
-    public Iterable<Alumno> findAll() {
-        return repository.findAll();
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public Optional<Alumno> findById(Long id) {
-        return repository.findById(id);
-    }
-
-    @Override
-    @Transactional
-    public Alumno save(Alumno alumno) {
-        return repository.save(alumno);
-    }
-
-    @Override
-    @Transactional
-    public void deleteById(Long id) {
-        repository.deleteById(id);
-    }
 }
